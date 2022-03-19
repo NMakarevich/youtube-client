477e74651434406002943640f70573ce3bb480e8
Nikolai Makarevich <nikoladze529@mail.ru>
2022-03-19 22:57:35 +0300

    feat: update app module

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ResultListComponent } from './result-list/result-list.component';
+import { ResultItemComponent } from './result-list/result-item/result-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ResultListComponent,
+    ResultItemComponent
   ],
   imports: [
     BrowserModule,
