68f1708855ad0ea5fad3e324816f9b7a37565044
Nikolai Makarevich <nikoladze529@mail.ru>
2022-03-25 12:07:18 +0300

    feat: add angular material

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ResultListComponent } from './result-list/result-list.component';
 import { ResultItemComponent } from './result-list/result-item/result-item.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, ResultListComponent, ResultItemComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
