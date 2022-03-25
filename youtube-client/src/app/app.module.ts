b50a5938ddc0663af7053aa0a94d4b31052f375f
Nikolai Makarevich <nikoladze529@mail.ru>
2022-03-25 16:24:55 +0300

    feat: add shared and material modules

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -7,10 +7,11 @@ import { HeaderComponent } from './header/header.component';
 import { ResultListComponent } from './result-list/result-list.component';
 import { ResultItemComponent } from './result-list/result-item/result-item.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, ResultListComponent, ResultItemComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
