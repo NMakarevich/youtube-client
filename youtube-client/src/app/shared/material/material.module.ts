bd37b52c6672ba4c5fbd53781efd3dd7a7614f22
Nikolai Makarevich <nikoladze529@mail.ru>
2022-03-27 00:08:53 +0300

    feat: add modules

--- youtube-client/src/app/shared/material/material.module.ts
+++ youtube-client/src/app/shared/material/material.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatCardModule],
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatCardModule,
+  ],
 })
 export class MaterialModule {}
