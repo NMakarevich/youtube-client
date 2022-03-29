1f026aca0b3d4b24ae929311fc2e0d1468af8b36
Nikolai Makarevich <nikoladze529@mail.ru>
2022-03-29 22:30:02 +0300

    feat: add render cards after click search button

--- youtube-client/src/app/header/header.component.ts
+++ youtube-client/src/app/header/header.component.ts
@@ -8,12 +8,21 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class HeaderComponent {
   @Input() isOpened: boolean = false;
 
+  public searchTerm: string = '';
+
   @Output() isOpenedChange = new EventEmitter<boolean>();
+
+  @Output() search = new EventEmitter<string>();
 
   public userName = 'User Name';
 
   toggle() {
     this.isOpened = !this.isOpened;
     this.isOpenedChange.emit(this.isOpened);
+  }
+
+  searchVideos() {
+    console.log(this.searchTerm);
+    if (this.searchTerm) this.search.emit(this.searchTerm);
   }
 }
