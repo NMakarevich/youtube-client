e2aebcea0ca98a38f9252f78fc24ff8ab77170be
Nikolai Makarevich <nikoladze529@mail.ru>
2022-04-02 13:22:06 +0300

    fix: fixes according recommendations in PR

--- youtube-client/src/app/pipes/sorting/sorting.pipe.ts
+++ youtube-client/src/app/pipes/sorting/sorting.pipe.ts
@@ -6,23 +6,21 @@ import { ResponseItem } from '../../interfaces/response-item';
 })
 export class SortingPipe implements PipeTransform {
   transform(results: ResponseItem[], sortTerm: string): ResponseItem[] {
-    if (!results || !sortTerm) return results;
+    if (!results || !results.length || !sortTerm) return results;
 
-    const sortParams = sortTerm.split('-');
-    const sortCriteria = sortParams[0];
-    const sortDirection = sortParams[1] === 'up' ? 1 : -1;
+    const [sortCriteria, sortDirection] = sortTerm.split('-');
 
     return results.sort((a, b) => {
       if (sortCriteria === 'date') {
         const aDate = new Date(a?.snippet?.publishedAt).getTime();
         const bDate = new Date(b?.snippet?.publishedAt).getTime();
-        return (aDate - bDate) * sortDirection;
+        return (aDate - bDate) * (sortDirection === 'up' ? 1 : -1);
       }
 
       if (sortCriteria === 'views') {
         const aViews = Number(a?.statistics?.viewCount);
         const bViews = Number(b?.statistics?.viewCount);
-        return (aViews - bViews) * sortDirection;
+        return (aViews - bViews) * (sortDirection === 'up' ? 1 : -1);
       }
 
       return 0;
