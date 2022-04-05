440f3165dca6cdcba4f28c1eb9ef775e82a03006
Nikolai Makarevich <nikoladze529@mail.ru>
2022-04-05 20:54:14 +0300

    refactor: move methods from components to service

--- youtube-client/src/app/core/components/header/header.component.ts
+++ youtube-client/src/app/core/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { YoutubeService } from '../../../youtube/services/youtube.service';
 
 @Component({
@@ -7,24 +7,7 @@ import { YoutubeService } from '../../../youtube/services/youtube.service';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  @Output() isOpenedChange = new EventEmitter<boolean>();
-
-  @Output() search = new EventEmitter<string>();
-
-  public isOpened = this.youtubeService.filterTerm
-
-  public searchTerm: string = '';
-
   public userName = 'User Name';
 
   constructor(public youtubeService: YoutubeService) {}
-
-
-  toggle() {
-    this.youtubeService.toggleFilterState()
-  }
-
-  searchVideos() {
-    if (this.searchTerm) this.search.emit(this.searchTerm);
-  }
 }
