303fdf87ca0c5ece14203449520babd365ef6884
Nikolai Makarevich <nikoladze529@mail.ru>
2022-04-06 21:52:09 +0300

    fix: fix code formatting

--- youtube-client/src/app/auth/auth.module.ts
+++ youtube-client/src/app/auth/auth.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagesModule } from './pages/pages.module';
-
-
+import { AuthService } from './services/auth.service';
 
 @NgModule({
-  declarations: [
-  ],
-  imports: [
-    CommonModule,
-    PagesModule
-  ],
-  exports: [PagesModule]
+  declarations: [],
+  imports: [CommonModule, PagesModule],
+  exports: [PagesModule],
+  providers: [AuthService],
 })
-export class AuthModule { }
+export class AuthModule {}
