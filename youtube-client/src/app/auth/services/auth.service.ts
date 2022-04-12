fcaabaf285b2e382dd8ac576e2ab31ecd6251b59
Nikolai Makarevich <nikoladze529@mail.ru>
2022-04-12 23:25:40 +0300

    refactor: implement subject for login (not finished)

--- youtube-client/src/app/auth/services/auth.service.ts
+++ youtube-client/src/app/auth/services/auth.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { ReplaySubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private readonly router: Router) {}
+  public subject = new ReplaySubject<boolean>(1);
+
+  constructor(private readonly router: Router) {
+    const auth = localStorage.getItem('authorization');
+    if (auth) this.subject.next(true);
+    else this.subject.next(false);
+  }
 
   login() {
     localStorage.setItem('authorization', 'true');
     this.router.navigate(['search']);
+    this.subject.next(true);
   }
 }
