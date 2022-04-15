b71193707821b2dbada576102c2a5786908e3975
Nikolai Makarevich <nikoladze529@mail.ru>
2022-04-15 22:45:06 +0300

    feat: add CanLoad

--- youtube-client/src/app/core/guards/auth.guard.ts
+++ youtube-client/src/app/core/guards/auth.guard.ts
@@ -2,8 +2,11 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  CanLoad,
+  Route,
   Router,
   RouterStateSnapshot,
+  UrlSegment,
   UrlTree,
 } from '@angular/router';
 import { Observable } from 'rxjs';
@@ -11,7 +14,7 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
   constructor(private readonly router: Router) {}
 
   canActivate(
@@ -21,5 +24,14 @@ export class AuthGuard implements CanActivate {
     if (!localStorage.getItem('authorization')) return this.router.parseUrl('/auth');
     const isAutorized = JSON.parse(localStorage.getItem('authorization') as string);
     return isAutorized || this.router.parseUrl('/auth');
+  }
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[],
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (!localStorage.getItem('authorization')) return this.router.parseUrl('/404');
+    const isAutorized = JSON.parse(localStorage.getItem('authorization') as string);
+    return isAutorized || this.router.parseUrl('/404');
   }
 }
